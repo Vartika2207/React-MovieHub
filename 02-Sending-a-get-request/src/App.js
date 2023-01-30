fc1d5671233cd1b0cb75b876276351c92b8d1296
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-01-31 00:28:03 +0530

    Added react scripts to fetch GET request

---
+++ 02-Sending-a-get-request/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  //sending HTTP request from inside react-app 
+  function fetchMovieHandler() {
+    // starts the process of fetching a resource from the network and return promise
+    // the 2nd argument allow us to pass js object where we can configure various options (i.e. extra headers, extra body, chanhe the http request)
+    // and by default GET request will be send
+    fetch('https://swapi.dev/api/films/').then(response => {
+      // response objc has a build-in method which automatically translate JSON to js object and it returns a promise
+    return response.json();
+    }).then(data => {
+      //this data is transformed data fro URL which will have a structure
+      // we used did some transformation because the file naming and the props.naming is diffrent
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+      setMovies(transformedMovies);
+    })
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
